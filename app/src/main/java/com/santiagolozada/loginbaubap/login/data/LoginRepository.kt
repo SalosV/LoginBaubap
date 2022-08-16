8685018da128e46a683f0924eb6fa53097e20703
Santiago Lozada <santiagolozada95@gmail.com>
2022-08-16 21:10:22 +0200

    Login with data fake

--- app/src/main/java/com/santiagolozada/loginbaubap/login/data/LoginRepository.kt
+++ app/src/main/java/com/santiagolozada/loginbaubap/login/data/LoginRepository.kt
@@ -2,7 +2,6 @@ package com.santiagolozada.loginbaubap.login.data
 
 import com.santiagolozada.loginbaubap.login.data.model.LoggedInUser
 
-
 class LoginRepository(private val dataSource: LoginDataSource) {
 
     var user: LoggedInUser? = null
